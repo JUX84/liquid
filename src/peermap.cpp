74fafa89850a7e3c5e30d95375ef0d773ef039f2
JUX84 <mehdi.mhiri@gmail.com>
2014-11-16 22:47:05 +0100

    fix hexip assigning

--- src/peermap.cpp
+++ src/peermap.cpp
@@ -21,7 +21,7 @@ void PeerMap::addPeer(const request& req) {
 		u = RequestHandler::getUser(req.at("passkey"));
 	else
 		u = new User();
-	if (pMap.find(req.at("peer_id")) == pMap.end())
+	if (u->getHexIP()->find(req.at("ip")+":"+req.at("port")) == u->getHexIP()->end())
 		u->addHexIP(req);
 	pMap.emplace(req.at("peer_id"), u);
 }
