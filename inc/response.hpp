1e92211b819cd6b1eb507008cc571191bb519f8d
JUX84 <mehdi.mhiri@gmail.com>
2015-06-23 17:19:43 +0200

    no names in inc

--- inc/response.hpp
+++ inc/response.hpp
@@ -3,5 +3,5 @@
 #include <string>
 
 std::string responseHead ();
-std::string response (const std::string&, bool gzip = false);
+std::string response (const std::string&, bool = false);
 std::string error (const std::string&);
